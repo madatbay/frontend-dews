8a0be73652a7563d92e37ee7e9b0da44ffe1a861
Madat Bay <medet.bayramov22@gmail.com>
2022-10-29 01:00:24 +0400

    add(auth): routing add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { ProtectedRoute } from "./hooks/ProtectedRoute";
+import FeedPage from "./pages/FeedPage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/feed"
+        element={
+          <ProtectedRoute>
+            <FeedPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./useAuth";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </NextUIProvider>
   </React.StrictMode>
 );
